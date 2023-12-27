42de6e9ff572b88799511ea03497381faf69d247
Nabil-Mabrouk <nabil.mabrouk.ai@gmail.com>
2023-12-27 19:58:52 +0100

    version 4 -persist logs bug1

--- GxxG/GxxG/settings.py
+++ GxxG/GxxG/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # new
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
 
 MEDIA_URL = '/media/' # new
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # new
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles') # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
